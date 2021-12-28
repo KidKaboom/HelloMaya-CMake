be8073f65c22ed140333fd67d8731b899830df86
Kid Kaboom <jt.tirado@gmail.com>
2021-12-27 19:04:33 -0800

    Adding initial files.

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <maya\MSimple.h>
+#include <maya\MGlobal.h>
+
+DeclareSimpleCommand(HelloMayaCmake, "Justin Tirado", "1.0")
+
+MStatus HelloMayaCmake::doIt(const MArgList& args) {
+    MGlobal::displayInfo("Hello Maya Cmake!");
+    return MS::kSuccess;
+}
